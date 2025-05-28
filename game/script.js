dcdeb5eae58dfb29825f705753e072f887e0b9f8
test <koush0811@gmail.com>
2025-05-28 18:09:02 +0900

    愛総工のホームページ

---
+++ game/script.js
@@ -0,0 +1,8 @@
+const text1 = "■";
+const text2 ="□";
+document.addEventListener('keydown',function(k){
+  if(k.key === "1"){
+    document.getElementById("waku1").style.backgroundColor = "black";
+  }
+  
+});
